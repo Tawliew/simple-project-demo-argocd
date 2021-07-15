e75dc3f332ff8d7dc46b9398bd51c2666a394342
Luiz Fernando <lfluiz.lf@gmail.com>
2021-07-15 00:56:23 -0300

    adicionada rota /port, retornando a porta que esta escutando

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const   express = require('express');
 const   app     = express();
-const   port    = process.env.NODEPORT || 80
+const   port    = process.env.NODEPORT || 8081
 
 app.get('/', (req, res) => 
 {
-    res.sendFile(__dirname + "/index.html");  
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.get('/port', (req, res) => 
+{
+    res.send(`{"port": "${port}"}`)
 });
 
 app.listen(port, () =>
